624136a16047c6c27b445a866dfb117dd74bddca
sivan73 <149456197+sivan73@users.noreply.github.com>
2024-10-06 23:21:19 +0530

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -38,7 +38,7 @@ public class ChatServer extends WebSocketServer {
     }
 
     public static void main(String[] args) {
-        int port = 8887; // Default port
+        int port = 8887; 
         ChatServer server = new ChatServer(port);
         server.start();  // Start the server without arguments
     }
